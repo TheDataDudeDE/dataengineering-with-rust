2e26b76b0fa8a355ea5f0338eca5d24cef382598
Markus (Abdullah) Clauss <markus@thedatadude.de>
2024-05-23 18:36:09 +0000

    created subproject

---
+++ print-data-structure/target/debug/deps/print_data_structure-69f5020fc804d695.d
@@ -0,0 +1,8 @@
+/workspaces/dataengineering-with-rust/print-data-structure/target/debug/deps/print_data_structure-69f5020fc804d695.rmeta: src/main.rs Cargo.toml
+
+/workspaces/dataengineering-with-rust/print-data-structure/target/debug/deps/print_data_structure-69f5020fc804d695.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
---
+++ print-data-structure/target/debug/print-data-structure.d
@@ -0,0 +1,1 @@
+/workspaces/dataengineering-with-rust/print-data-structure/target/debug/print-data-structure: /workspaces/dataengineering-with-rust/print-data-structure/src/main.rs
