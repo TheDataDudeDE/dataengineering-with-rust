305a2309a4aa79131e4aec2d3a4d0749d1778443
Markus (Abdullah) Clauss <markus@thedatadude.de>
2024-05-23 21:31:43 +0000

    vecdeque vector fruit salad

---
+++ vecdeque-fruit-salad/target/debug/deps/vecdeque_fruit_salad-a8735659cbd24858.d
@@ -0,0 +1,5 @@
+/workspaces/dataengineering-with-rust/vecdeque-fruit-salad/target/debug/deps/vecdeque_fruit_salad-a8735659cbd24858: src/main.rs
+
+/workspaces/dataengineering-with-rust/vecdeque-fruit-salad/target/debug/deps/vecdeque_fruit_salad-a8735659cbd24858.d: src/main.rs
+
+src/main.rs:
---
+++ vecdeque-fruit-salad/target/debug/vecdeque-fruit-salad.d
@@ -0,0 +1,1 @@
+/workspaces/dataengineering-with-rust/vecdeque-fruit-salad/target/debug/vecdeque-fruit-salad: /workspaces/dataengineering-with-rust/vecdeque-fruit-salad/src/main.rs
