1eb288562a23f75636029e64abaabd5ef6e2f660
Markus (Abdullah) Clauss <markus@thedatadude.de>
2024-05-25 09:33:43 +0000

    dropped target file

--- vector-fruit-salad/target/debug/deps/vector_fruit_salad-9f7ef5305a73e99f.d
+++
@@ -1,5 +0,0 @@
-/workspaces/dataengineering-with-rust/vector-fruit-salad/target/debug/deps/vector_fruit_salad-9f7ef5305a73e99f: src/main.rs
-
-/workspaces/dataengineering-with-rust/vector-fruit-salad/target/debug/deps/vector_fruit_salad-9f7ef5305a73e99f.d: src/main.rs
-
-src/main.rs:
--- vector-fruit-salad/target/debug/vector-fruit-salad.d
+++
@@ -1,1 +0,0 @@
-/workspaces/dataengineering-with-rust/vector-fruit-salad/target/debug/vector-fruit-salad: /workspaces/dataengineering-with-rust/vector-fruit-salad/src/main.rs
