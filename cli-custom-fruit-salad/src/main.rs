16b5a03b197a0b2b8fd040c270ef3f8a44d51300
Markus (Abdullah) Clauss <markus@thedatadude.de>
2024-05-28 22:53:38 +0200

    added csv output

--- cli-custom-fruit-salad/src/main.rs
+++ cli-custom-fruit-salad/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use cli_custom_fruit_salad::create_fruit_salad;
-use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::io::Write;
 
 #[derive(Parser)]
 #[clap(version = "1.0", author = "Your Name")]
@@ -13,28 +14,40 @@ struct Opts {
     csvfile: Option<String>,
     #[clap(short = 'd', long = "dressing")]
     dressing: Option<String>,
+    #[clap(short = 'o', long = "output")]
+    output: Option<String>,
 }
 
 //Function that converts a csv-file to a vector of strings
 fn csv_to_vec(csv: &str) -> Vec<String> {
-    csv.split(',')
-        .map(|s| s.trim().to_string())
-        .collect()
+    csv.split(',').map(|s| s.trim().to_string()).collect()
 }
 
-
 //Display the fruit salad
-fn display_fruit_salad(fruits: Vec<String>, dressing: String) {
-    if fruits.len() > 1{
+fn display_fruit_salad(fruits: &Vec<String>, dressing: &String) {
+    if fruits.len() > 1 {
         println!("Your fruit salad contains: ");
-        for fruit in &fruits {
+        for fruit in fruits {
             println!("{}", fruit);
         }
-            println!("With delicious dressing {}", dressing)
-    }
-    else {
+        println!("With delicious dressing {}", dressing)
+    } else {
         println!("You need to add fruits to your salad");
     }
+}
+
+fn write_fruit_salad_to_csv(
+    fruits: Vec<String>,
+    dressing: String,
+    output: &str,
+) -> std::io::Result<()> {
+    let mut file = std::fs::File::create(output)?;
+    writeln!(file, "Fruit:")?;
+    for fruit in fruits {
+        writeln!(file, "{}", fruit)?;
+    }
+    writeln!(file, "Dressing: {}", dressing)?;
+    Ok(())
 }
 
 //Add dressing
@@ -48,22 +61,23 @@ fn main() {
     let opts = Opts::parse();
     let fruit_list = match opts.csvfile {
         Some(filename) => {
-            let fruits = std::fs::read_to_string(filename)
-            .expect("Could not read file");
+            let fruits = std::fs::read_to_string(filename).expect("Could not read file");
             csv_to_vec(&fruits)
-
-        },
-        None => {
-            opts
+        }
+        None => opts
             .fruits
             .unwrap_or_default()
             .split(',')
             .map(|s| s.trim().to_string())
-            .collect()
-        },
+            .collect(),
     };
 
     let dressing = opts.dressing.unwrap_or_else(|| random_dressing());
     let fruit_salad = create_fruit_salad(fruit_list);
-    display_fruit_salad(fruit_salad, dressing);
+    display_fruit_salad(&fruit_salad, &dressing);
+
+    if let Some(output_file) = opts.output {
+        write_fruit_salad_to_csv(fruit_salad, dressing, &output_file)
+            .expect("Could not write to file");
+    }
 }
